973257fd23ab8be2fd45f5093d881baa608b1df0
Amol Kshirsagar <kshirsagar.amol.r@gmail.com>
2023-09-26 22:41:40 +1000

    label changes

--- src/App.js
+++ src/App.js
@@ -10,70 +10,70 @@ function App() {
       img: 'https://placehold.co/200x100',
       title: 'Card Title 1',
       description: '',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:2,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 2',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:3,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 3',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:4,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 4',
       description: 'Lorem ipsum dolor sit amet',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:5,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 5',
       description: 'Lorem ipsum dolor sit amet',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:6,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 6',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:7,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 7',
       description: 'Lorem ipsum',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:8,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 8',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:9,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 9',
       description: 'Lorem',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
     {
       id:10,
       img: 'https://placehold.co/200x100',
       title: 'Card Title 10',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      buttonText: 'Read More',
+      buttonText: 'BUTTON',
     },
   ];
 
@@ -97,7 +97,7 @@ function App() {
             </div>
             <div style={{ ...generateGridStyles(3, index + 1), marginBottom: 10, backgroundColor:'#808080' }}><strong>{product.title}</strong></div>
             <div style={{ ...generateGridStyles(4, index + 1), marginBottom: 10,backgroundColor:'#C0C0C0' }}>{product.description}</div>
-            <div style={{ ...generateGridStyles(5, index + 1),backgroundColor:'#696969' }}><button>{product.buttonText}</button></div>
+            <div style={{ ...generateGridStyles(5, index + 1),backgroundColor:'#696969',display:'flex',justifyContent:'space-between' }}><button>{product.buttonText} 1</button><button>{product.buttonText} 2</button></div>
           </div>
         ))}
       </div>
